e84babdb58a94b51e9c36fb3fa40cffd400fac56
gperuzzi <guilherme_peruzzi@msn.com>
2017-07-28 15:50:27 +0100

    d

--- EncriptCSVFiles/Program.cs
+++ EncriptCSVFiles/Program.cs
@@ -11,7 +11,7 @@ namespace EncriptCSVFiles
     public class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             try 
             {
                 foreach (string s in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.csv").Select(System.IO.Path.GetFileName))
